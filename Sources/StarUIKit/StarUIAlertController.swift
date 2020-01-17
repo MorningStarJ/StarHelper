cb7c3e55ed567a6419e1bacb4b753d4108dfc5cd
MorningStar <jngviho@icloud.com>
2020-01-17 23:12:30 +0800

    changed: update handler parameter

--- Sources/StarUIKit/StarUIAlertController.swift
+++ Sources/StarUIKit/StarUIAlertController.swift
@@ -11,11 +11,11 @@ public extension UIViewController {
     func alert(title: String? = nil,
                        message: String? = nil,
                        style: UIAlertController.Style = .alert,
-                       actions: (actionTitle: String, style: UIAlertAction.Style, handler: (() -> Void)?)...) {
+                       actions: (actionTitle: String, style: UIAlertAction.Style, handler: ((UIAlertAction) -> Void)?)...) {
         let alertVc = UIAlertController(title: title, message: message, preferredStyle: style)
         actions.forEach { actionDetail in
-            let action = UIAlertAction(title: actionDetail.actionTitle, style: actionDetail.style) { _ in
-                actionDetail.handler?()
+            let action = UIAlertAction(title: actionDetail.actionTitle, style: actionDetail.style) { action in
+                actionDetail.handler?(action)
             }
             alertVc.addAction(action)
         }
