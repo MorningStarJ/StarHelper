583d1bed9a7fd2645839d69d492e33bb16453954
MorningStar <jngviho@icloud.com>
2020-05-06 22:38:58 +0800

    add font && tableview shortcut

---
+++ Sources/StarUIKit/StarUIFont.swift
@@ -0,0 +1,24 @@
+//
+//  StarUIFont.swift
+//  
+//
+//  Created by MorningStar on 2020/5/6.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    enum Style: String {
+        case light = "Light"
+        case medium = "Medium"
+        case regular = "Regular"
+        case semibold = "Semibold"
+        case thin = "Thin"
+        case ultralight = "Ultralight"
+    }
+    
+    static func pingfangSC(style: Style = .regular, fontSize: CGFloat) -> UIFont {
+        return UIFont(name: "PingFangSC-\(style.rawValue)", size: fontSize)!
+    }
+}
--- Sources/StarUIKit/StarUIStoryboard.swift
+++
@@ -1,8 +0,0 @@
-//
-//  StarUIStoryboard.swift
-//  
-//
-//  Created by MorningStar on 2019/12/17.
-//
-
-import Foundation
