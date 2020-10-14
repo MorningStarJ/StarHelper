03847b839ff9227fd678d9e90fbe73700c20f9a2
MorningStar <jngviho@icloud.com>
2020-10-15 05:23:54 +0800

    fix UIFont extension

--- Sources/StarUIKit/StarUIFont.swift
+++ Sources/StarUIKit/StarUIFont.swift
@@ -8,8 +8,8 @@
 import Foundation
 import UIKit
 
-public extension UIFont {
-    enum Style: String {
+extension UIFont {
+    public enum Style: String {
         case light = "Light"
         case medium = "Medium"
         case regular = "Regular"
@@ -18,7 +18,7 @@ public extension UIFont {
         case ultralight = "Ultralight"
     }
     
-    static func pingfangSC(style: Style = .regular, fontSize: CGFloat) -> UIFont {
+    static public func pingfangSC(style: Style = .regular, fontSize: CGFloat) -> UIFont {
         return UIFont(name: "PingFangSC-\(style.rawValue)", size: fontSize)!
     }
 }
